597019ebdbb3d2157af630c342ae7904df4af9b8
Bump Dolwijit <42529403+dolwijit13@users.noreply.github.com>
2026-07-30 23:10:59 +0700

    fix: resolve DragonRuby mRuby rand_range compatibility in app/enemy_spawner.rb (#14)

--- app/enemy_spawner.rb
+++ app/enemy_spawner.rb
@@ -8,7 +8,7 @@ class EnemySpawner
   def initialize(min_interval = 2.0, max_interval = 3.0)
     @min_interval = min_interval.to_f
     @max_interval = max_interval.to_f
-    @spawn_timer = rand(@min_interval..@max_interval)
+    reset_timer
   end
 
   def update(delta_time = 1.0 / 60.0, boundary_width = 1280, boundary_height = 720)
@@ -16,7 +16,8 @@ class EnemySpawner
 
     if @spawn_timer <= 0
       reset_timer
-      spawn_y = rand(0..(boundary_height - EvilCat::HEIGHT)).to_f
+      max_y = (boundary_height - EvilCat::HEIGHT).to_f
+      spawn_y = rand_range(0.0, max_y)
       EvilCat.new(boundary_width, spawn_y)
     else
       nil
@@ -24,6 +25,12 @@ class EnemySpawner
   end
 
   def reset_timer
-    @spawn_timer = rand(@min_interval..@max_interval)
+    @spawn_timer = rand_range(@min_interval, @max_interval)
+  end
+
+  private
+
+  def rand_range(min, max)
+    rand * (max - min) + min
   end
 end

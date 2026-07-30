f85ed8246688021621c20bb3d482bcdf555bf522
Bump Dolwijit <42529403+dolwijit13@users.noreply.github.com>
2026-07-30 19:27:03 +0700

    feat: integrate camera side-scrolling and 8-direction controls in main.rb (#3)

--- main.rb
+++ main.rb
@@ -2,22 +2,35 @@
 
 require 'gosu'
 require_relative 'lib/player'
+require_relative 'lib/camera'
+require_relative 'lib/input_handler'
 
 class GameWindow < Gosu::Window
   def initialize
     super 800, 600
     self.caption = "Dog Dash Drift"
 
-    @player = Player.new
+    @player = Player.new(100, 284)
+    @camera = Camera.new(1.5)
   end
 
   def update
-    @player.update(mouse_x, mouse_y)
+    @camera.update
+    @player.update(self, width, height)
   end
 
   def draw
     # Background color
     Gosu.draw_rect(0, 0, width, height, Gosu::Color.argb(0xff_1e1e2e))
+
+    # Grid background lines scrolling effect
+    grid_spacing = 40
+    offset_x = (@camera.x % grid_spacing).to_i
+
+    ((width / grid_spacing) + 2).times do |i|
+      x_pos = (i * grid_spacing) - offset_x
+      Gosu.draw_line(x_pos, 0, Gosu::Color.argb(0x22_ffffff), x_pos, height, Gosu::Color.argb(0x22_ffffff))
+    end
 
     @player.draw
   end
